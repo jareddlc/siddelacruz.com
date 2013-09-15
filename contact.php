c0de38e9d24a12b485858c572c3db422528d850d
Jared <jared_de_la_cruz@yahoo.com>
2013-09-15 16:45:59 -0700

    added clients, fixed email

--- contact.php
+++ contact.php
@@ -2,14 +2,15 @@
 
 $name = trim($_POST['name']);
 $email = trim($_POST['email']);
+$text = trim($_POST['text']);
 
 if(!empty($name) && !empty($email))
 {
   // Confirmation mail
   $to = "sid@siddelacruz.com";
   $subject = "Contact Email";
-  $message = trim($_POST['text']);
-  $headers = $email . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+  $message = "From: " . $name . "\r\n" . "Email: " . $email . "\r\n" . "Message:\r\n" . $text;
+  $headers = "From:" . $email;
   mail($to, $subject, $message, $headers);
   echo json_encode("200");
 }
